e37dcc8cb804bedac15a82b3188161c4395e6a99
Bearded Avenger <email@nickhaskins.com>
2014-06-19 10:40:20 -0500

    getting started guide

--- templates/archive-ase_docs.php
+++ templates/archive-ase_docs.php
@@ -8,21 +8,28 @@ get_header();
 
 		echo '<div class="ase-docs-right">';
 			?>
-			<div class="ase-docs-welcome-block ase-docs-core-welcome">
-				<h5>ASE Owners Manual</h5>
-				<p>All the information you could possibly need about Aesop Story Engine and it's options</p>
-			</div>
-			<div class="ase-docs-welcome-block ase-docs-theme-welcome">
-				<h5>ASE Theme Documentation</h5>
-				<p>Here you'll find documentation for the various themes developed by Aesopinteractive.</p>
-			</div>
-			<div class="ase-docs-welcome-block ase-docs-addon-welcome">
-				<h5>ASE Addon Documentation</h5>
-				<p>Addons from the library and all of their documentation can be found here.</p>
-			</div>
-			<div class="ase-docs-welcome-block ase-docs-dev-welcome">
-				<h5>Information for Developers</h5>
-				<p>If you're a developer looking for filters and actions, this is the place for you.</p>
+			<p class="lead">Welcome to the Aesop Story Engine Help Center. Below you'll find a guide on getting Aesop Story Engine installed, and to the left you'll find documentation for Aesop Story Engine, as well as all of our products.</p>
+			<div class="ase-quick-start">
+
+				<div class="ase-section ase-step-one">
+					<div class="clearfix">
+						<div class="ase-step-inner">
+							<h2>Install Aesop Story Engine</h2>
+							<p>Head to the plugins tab within your WordPress administration area. Click on "add new". Once here, search for Aesop Story Engine, and press "search." Install Aesop Story Engine, and you're set! Hint: you can also install through FTP if you know that that is.</p>
+						</div>
+						<?php echo do_shortcode('[aesop_image img="/wp-content/themes/ase/assets/img/install-aesop.png" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
+					</div>
+				</div>
+
+				<div class="ase-section ase-step-two">
+					<div class="clearfix">
+						<div class="ase-step-inner">
+							<h2>Build your Story</h2>
+							<p>Go to any post, and locate the "Add Component" button. It sits right above the post editor. Once you toggle this, you'll be able to select the component, adjust any options, and insert it anywhere within your story. Galleries are managed with the "Galleries" tab within WordPress.</p>
+						</div>
+						<?php echo do_shortcode('[aesop_image img="/wp-content/themes/ase/assets/img/add-component.png" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
+					</div>
+				</div>
 			</div>
 			<?php
 		echo '</div>';
