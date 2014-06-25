789041c945b8c210e23d11322c2a67465765d9ee
Bearded Avenger <email@nickhaskins.com>
2014-06-25 16:37:13 -0500

    better search results template

--- templates/archive-search.php
+++ templates/archive-search.php
@@ -6,15 +6,25 @@ get_header();
 
 	?><div class="ase-content ase-docs ase-docs-search">
 
-        <?php if ( have_posts() ) : 
-        	while ( have_posts() ) : the_post();
-        		?><a href="<?php the_permalink();?>"><?php the_title(); ?></a><?php
-    		endwhile;
-    	else:
-    		?><p>We're sorry but nothing comes up for that search. Try searching again, or head <a href="/help">back</a> to the main help screen.</p><?php
-    	endif;
-
-    	?>
+		<?php echo ase_docs_crumbs();?>
+		
+		<ul class="unstyled">
+	        <?php if ( have_posts() ) : 
+	        	while ( have_posts() ) : the_post();
+	        		?><li>
+	        				<a href="<?php the_permalink();?>">
+	        					<?php the_title('<h6>','</h6>'); ?>
+	        					<div class="ase-docs-search-result-content">
+	        						<?php echo wp_trim_words(get_the_content(), 38, '...');?>
+	        					</div>
+	        				</a>
+	        			</li><?php
+	    		endwhile;
+	   	?></ul><?php
+	    	else:
+	    		?><p>We're sorry but nothing comes up for that search. Try searching again, or head <a href="/help">back</a> to the main help screen.</p><?php
+	    	endif;
+	    	?>
     </div>
 
 <?php
